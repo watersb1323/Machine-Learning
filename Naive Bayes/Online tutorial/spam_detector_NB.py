53b53bd2551873cb4b0b6bb27eb0e53e941b84d9
watersb1323 <watersb1323@gmail.com>
2017-07-25 19:16:45 +0100

    New version with extract_features function

--- Naive Bayes/Online tutorial/spam_detector_NB.py
+++ Naive Bayes/Online tutorial/spam_detector_NB.py
@@ -2,6 +2,7 @@ __author__ = "Brian Waters"
 
 import os
 from collections import Counter
+import numpy as np
 
 
 def make_Dictionary(train_dir):
@@ -29,9 +30,31 @@ def print_dict(dict_to_print):
     for k,v in dict_to_print.items():
         print("%s - %s" % (str(k), str(v)))
 
+def extract_features(mail_dir):
+    files = [os.path.join(mail_dir, fi) for fi in os.listdir(mail_dir)]
+    features_matrix = np.zeros((len(files),3000))
+    docID = 0
+    for fil in files:
+        with open(fil) as fi:
+            for i,line in enumerate(fi):
+                if i == 2:
+                    words = line.split()
+                    for word in words:
+                        wordID = 0
+                        for j, d in enumerate(dictionary):
+                            if d[j] == word:
+                                wordID = j
+                                features_matrix[docID, wordID] = words.count(word)
+            docID = docID + 1
+    return features_matrix
+
+
+
 if __name__ == "__main__":
     train_dir = r"C:\Users\brwaters\Documents\GitHub\Machine-Learning\Naive Bayes\Online tutorial\train-mails"
 
     my_dict = make_Dictionary(train_dir)
+
+    train_matrix = extract_features(train_dir)
 
     print(my_dict)
